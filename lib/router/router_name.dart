a007c387ff4348f42b5e4c36c6aa254ac60be9aa
prasetyanurangga <angganurprasetya4@gmail.com>
2022-06-24 02:02:23 +0700

    ☺

--- lib/router/router_name.dart
+++ lib/router/router_name.dart
@@ -1,6 +1,6 @@
 class RoutesName {
-  static const String RESULT_PAGE = '/result';
-  static const String LOGIN_PAGE = '/login';
-  static const String LANDING_PAGE = '/';
-  static const String SHARE_PAGE = '/share';
+  static const String RESULT_PAGE = 'result';
+  static const String LOGIN_PAGE = 'login';
+  static const String LANDING_PAGE = 'landing';
+  static const String SHARE_PAGE = 'share';
 }
