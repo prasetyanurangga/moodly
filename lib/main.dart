6fc9f439682c649baf3fde1dce861b5dc194819f
prasetyanurangga <angganurprasetya4@gmail.com>
2022-06-24 02:52:32 +0700

    ☺

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ import 'package:url_strategy/url_strategy.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   setPathUrlStrategy();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
